2e831114c479eaad59149cddb83bd7df7c3ac1a2
Ezerike Caleb Chikere <cezerike@gmail.com>
2024-01-24 18:12:43 +0100

    'updates: fixed dropdown issues'

--- components/SubHeader.js
+++ components/SubHeader.js
@@ -1,10 +1,39 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 
 const MensWear = () => {
   const [isDropdownOpen, setDropdownOpen] = useState(false)
+  const dropdownRef = useRef(null)
+
+  useEffect(() => {
+    // Attach the event listeners to the container
+    const container = dropdownRef.current
+
+    const openDropdown = () => {
+      setDropdownOpen(true)
+    }
+
+    const closeDropdown = () => {
+      setDropdownOpen(false)
+    }
+
+    if (container) {
+      container.addEventListener('mouseenter', openDropdown)
+      container.addEventListener('mouseleave', closeDropdown)
+    }
+
+    return () => {
+      // Cleanup event listeners
+      if (container) {
+        container.removeEventListener('mouseenter', openDropdown)
+        container.removeEventListener('mouseleave', closeDropdown)
+      }
+    }
+  }, [])
 
   return (
-    <div className='group relative cursor-pointer flex items-center'>
+    <div
+      className='h-[50px] cursor-pointer flex items-center '
+      ref={dropdownRef}>
       <span className='mr-2'>Menswear</span>
       <svg
         xmlns='http://www.w3.org/2000/svg'
@@ -14,9 +43,7 @@ const MensWear = () => {
         stroke='currentColor'
         className={`w-6 h-6 mb-1  ${
           isDropdownOpen ? 'rotate-180 pb-2' : 'pt-2'
-        }`}
-        onMouseEnter={() => setDropdownOpen(true)}
-        onMouseLeave={() => setDropdownOpen(false)}>
+        }`}>
         <path
           stroke-linecap='round'
           stroke-linejoin='round'
@@ -24,7 +51,7 @@ const MensWear = () => {
         />
       </svg>
       {isDropdownOpen && (
-        <ul className='absolute left-[50px] top-[30px] bg-white bg-opacity-75 p-2 shadow-md transition-all duration-500 ease-in-out mt-2'>
+        <ul className='w-[150px]  absolute left-[50px] top-[30px] bg-white bg-opacity-75 p-2 shadow-md transition-all duration-500 ease-in-out mt-2'>
           <li>Item 1</li>
           <li>Item 2</li>
           <li>Item 3</li>
@@ -38,9 +65,38 @@ const MensWear = () => {
 
 const WomensWear = () => {
   const [isDropdownOpen, setDropdownOpen] = useState(false)
+  const dropdownRef = useRef(null)
+
+  useEffect(() => {
+    // Attach the event listeners to the container
+    const container = dropdownRef.current
+
+    const openDropdown = () => {
+      setDropdownOpen(true)
+    }
+
+    const closeDropdown = () => {
+      setDropdownOpen(false)
+    }
+
+    if (container) {
+      container.addEventListener('mouseenter', openDropdown)
+      container.addEventListener('mouseleave', closeDropdown)
+    }
+
+    return () => {
+      // Cleanup event listeners
+      if (container) {
+        container.removeEventListener('mouseenter', openDropdown)
+        container.removeEventListener('mouseleave', closeDropdown)
+      }
+    }
+  }, [])
 
   return (
-    <div className='cursor-pointer flex items-center'>
+    <div
+      className='h-[50px] cursor-pointer flex items-center'
+      ref={dropdownRef}>
       <span className='mr-2'>Womenwear</span>
       <svg
         xmlns='http://www.w3.org/2000/svg'
@@ -52,7 +108,7 @@ const WomensWear = () => {
           isDropdownOpen ? 'rotate-180 pb-2' : 'pt-2'
         }`}
         onMouseEnter={() => setDropdownOpen(true)}
-        onMouseLeave={() => setDropdownOpen(false)}>
+        onMouseLeave={() => setDropdownOpen(true)}>
         <path
           stroke-linecap='round'
           stroke-linejoin='round'
@@ -74,7 +130,7 @@ const WomensWear = () => {
 
 const SubHeader = () => {
   return (
-    <div className='w-full mt-[100px] md:mt-[70px] mx-auto max-w-2xl flex justify-left items-left bg-white bg-opacity-75 p-4 z-50 xl:h-auto fixed border-2 border-red-600 border-solid'>
+    <div className='w-full mt-[100px] md:mt-[70px] mx-auto max-w-2xl flex justify-left items-left bg-white bg-opacity-75 p-2 z-50 xl:h-auto fixed border-2 border-red-600 border-solid'>
       <ul className='relative h-auto w-full mx-auto text-xl text-black md:h-auto md:py-auto md:items-left md:justify-left md:flex gap-x-[50px] border-2 border-red-600 border-solid'>
         <li className='group relative cursor-pointer flex items-center'>
           <MensWear />
@@ -82,8 +138,12 @@ const SubHeader = () => {
         <li className='flex items-center'>
           <WomensWear />
         </li>
-        <li className='hover:text-purple-600 text-gray-800'>Brands</li>
-        <li className='text-purple-600'>New Year Sale</li>
+        <li className='group relative cursor-pointer flex items-center'>
+          Brands
+        </li>
+        <li className='group relative cursor-pointer flex items-center text-purple-600'>
+          New Year Sale
+        </li>
       </ul>
     </div>
   )
