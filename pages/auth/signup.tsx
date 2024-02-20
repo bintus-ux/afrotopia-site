c1212eca1322a70f2e409942128f773ff34af91c
Ezerike Caleb Chikere <cezerike@gmail.com>
2024-02-20 14:36:54 +0100

    'update: validator reinf.'

--- pages/auth/signup.tsx
+++ pages/auth/signup.tsx
@@ -2,11 +2,19 @@ import React, { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
 import { supabase } from '../../supabaseClient'
 
-const SignUp = () => {
+const SignUp = ({ hasNumber, hasValidLength, hasCapitalLetter }) => {
   const router = useRouter()
   const [email, setEmail] = useState('')
   const [isValid, setIsValid] = useState(true)
   const [message, setMessage] = useState('')
+  const [hrBackground, setHrBackground] = useState([
+    'bg-gray-500',
+    'bg-gray-500',
+    'bg-gray-500',
+    'bg-gray-500',
+    'bg-gray-500',
+  ])
+  const [isSubmitDisabled, setIsSubmitDisabled] = useState(true)
 
   useEffect(() => {
     fetch('/api/hello')
@@ -25,6 +33,66 @@ const SignUp = () => {
     setIsValid(value.includes('@'))
   }
 
+  const handlePasswordChange = (e) => {
+    const { value } = e.target
+    setPassword(value)
+
+    // Check if password length is at least 8 characters
+    const hasValidLength = value.length >= 8
+
+    // Check if password contains at least 2 special characters and one capital letter
+    const hasSpecialCharacters =
+      value.match(/[!@#$%^&*(),.?":{}|<>]/g)?.length >= 2
+    const hasCapitalLetter = value.match(/[A-Z]/) !== null
+    const hasNumber = value.match(/[0-9]/) !== null
+
+    // Update hr background colors based on password conditions
+    if (
+      hasValidLength &&
+      hasSpecialCharacters &&
+      hasCapitalLetter &&
+      hasNumber
+    ) {
+      setHrBackground([
+        'bg-green-500',
+        'bg-green-500',
+        'bg-green-500',
+        'bg-green-500',
+        'bg-green-500',
+      ])
+      setIsSubmitDisabled(false) // Enable submit button if all conditions are met
+    } else if (hasNumber && hasCapitalLetter) {
+      // Check if password has special characters & capital letters
+      setHrBackground([
+        'bg-yellow-500',
+        'bg-yellow-500',
+        'bg-gray-500',
+        'bg-gray-500',
+        'bg-gray-500',
+      ])
+      setIsSubmitDisabled(true) // Disable submit button if password length is exactly 8 characters but other conditions are not met
+    } else if (value.length >= 8) {
+      // Check if password is exactly 8 characters in length
+      setHrBackground([
+        'bg-yellow-500',
+        'bg-yellow-500',
+        'bg-yellow-500',
+        'bg-gray-500',
+        'bg-gray-500',
+      ])
+      setIsSubmitDisabled(true) // Disable submit button if password length is exactly 8 characters but other conditions are not met
+    } else {
+      setHrBackground([
+        'bg-gray-500',
+        'bg-gray-500',
+        'bg-gray-500',
+        'bg-gray-500',
+        'bg-gray-500',
+      ])
+      setIsSubmitDisabled(true) // Disable submit button if any other condition is not met
+    }
+  }
+
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault()
 
@@ -39,6 +107,12 @@ const SignUp = () => {
       router.push('/auth/signin')
     }
   }
+  const hrElements = Array.from({ length: 5 }, (_, index) => (
+    <hr
+      key={index}
+      className={`w-10 h-3 border-t-0 rounded-md ${hrBackground[index]}`}
+    />
+  ))
 
   return (
     <div className='h-screen flex items-center justify-center bg-gray-800'>
@@ -73,13 +147,35 @@ const SignUp = () => {
               type='password'
               id='password'
               value={password}
-              onChange={(e) => setPassword(e.target.value)}
+              onChange={handlePasswordChange}
             />
+          </div>
+          <div className='flex flex-row justify-between items-center gap-x-3 mt-5'>
+            <div className='flex flex-row items-center gap-x-3'>
+              {hrElements.map((hr, index) => (
+                <div key={index}>{hr}</div>
+              ))}
+            </div>
+            {password && ( // Check if password is not empty
+              <>
+                {!hasNumber ? (
+                  <span className='text-red-500'>Weak</span>
+                ) : (
+                  <span className='text-green-500'>Good</span>
+                )}
+                {hasNumber && <span className='text-green-500'>Good</span>}
+              </>
+            )}
           </div>
 
           <button
-            className='mt-10 text-lg text-white font-semibold bg-green-500 py-3 px-6 rounded-md focus:outline-none focus:ring-2'
-            type='submit'>
+            className={`mt-10 text-lg text-white font-semibold py-3 px-6 rounded-md focus:outline-none focus:ring-2 ${
+              isSubmitDisabled
+                ? 'bg-green-700 cursor-not-allowed'
+                : 'bg-green-500 cursor-pointer'
+            }`}
+            type='submit'
+            disabled={isSubmitDisabled}>
             Sign up
           </button>
         </form>
